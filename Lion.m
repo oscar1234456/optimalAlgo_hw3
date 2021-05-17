809dba895fadafcd35adf420e8ef40cbb091b5d1
陳泰元 <chen.oscar@hotmail.com>
2021-05-17 23:54:52 +0800

    [Add]add Territorial Defense

--- Lion.m
+++ Lion.m
@@ -7,6 +7,7 @@ Pc_2 = 0.6;
 Pm = 0.5;
 
 %Data Variable & Structure Initialization
+nowAge = 0;
 nowCubs = {};
 nowCubsFitness = [];
 maleCubs = {};
@@ -17,7 +18,7 @@ Xm = getNewLion();
 Xf = getNewLion();
 
 %Step2. Mating
-%---Generate cubs from territorial male and female (Crossover)---
+%---Generate cubs from territorial male and female (Crossover & mutation)---
 crossoverCubs = crossover({Xm,Xf},Pc_1, Pc_2);
 mutationCubs = mutation(crossoverCubs, Pm);
 nowCubs = [crossoverCubs(1:4),mutationCubs(1:4)];
@@ -30,6 +31,8 @@ for i=2:length(nowCubs)
     vec = convLion2Value(nowCubs{i});
     location = cat(1,vec,location);
 end
+
+%---Kmeans to cluster cubs to male and female---
 [team,C] = kmeans(location, 2);
 
 for i=1:length(team)
@@ -43,10 +46,48 @@ for i=1:length(team)
 end
 
 if(length(maleCubs) ~= length(femaleCubs))
-
+    distance = abs(length(maleCubs)-length(femaleCubs));
+    if(length(maleCubs)>length(femaleCubs))
+        % male Cubs bigger than female Cubs
+        disp("Male Change");
+        tempMaleCubsFitness = [];
+        for i=1:length(maleCubs)
+            vec = convLion2Value(maleCubs{i});
+            tempMaleCubsFitness(end+1) = fitness(vec(1), vec(2));
+        end
+        for j=1:distance
+            [big, bigIndex] = max(tempMaleCubsFitness);
+            tempMaleCubsFitness(bigIndex) = [];
+            maleCubs(bigIndex) = [];
+        end
+    else
+        % female Cubs bigger than male Cubs
+         disp("Female Change");
+         tempFemaleCubsFitness = [];
+        for i=1:length(femaleCubs)
+            vec = convLion2Value(femaleCubs{i});
+            tempFemaleCubsFitness(end+1) = fitness(vec(1), vec(2));
+        end
+        for j=1:distance
+            [big, bigIndex] = max(tempFemaleCubsFitness);
+            tempFemaleCubsFitness(bigIndex) = [];
+            femaleCubs(bigIndex) = [];
+        end
+    end
 end
 
 %Step3. Territorial Defense
+nowAge = 0;
+nomalLion = getNewLion();
+vec =  convLion2Value(nomalLion);
+nomalLionFit = fitness(vec(1), vec(2));
+
+vec = convLion2Value(Xm);
+XmFit = fitness(vec(1), vec(2));
+ if(nomalLionFit < XmFit)
+    % nomal Lion win Xmale Lion
+    %compare with pride:
+ end
 
 
 
